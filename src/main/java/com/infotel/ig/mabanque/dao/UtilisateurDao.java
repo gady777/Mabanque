d08f5438155944027fdb11023e89fb6b92913f1e
Vincent Douwe <douwevincent@yahoo.fr>
2020-06-02 20:11:21 +0100

    Ajout des méthodes dans la classe UtilisateurDao

--- src/main/java/com/infotel/ig/mabanque/dao/UtilisateurDao.java
+++ src/main/java/com/infotel/ig/mabanque/dao/UtilisateurDao.java
@@ -1,7 +1,10 @@
 
 package com.infotel.ig.mabanque.dao;
 
+import com.infotel.ig.mabanque.entities.Role;
 import com.infotel.ig.mabanque.entities.Utilisateur;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -10,4 +13,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UtilisateurDao  extends JpaRepository<Utilisateur, Long>{
     
+   public Optional<Utilisateur> findByEmail(String email);
+   
+   public List<Utilisateur> findByNomLike(String nom);
+   
+   public List<Utilisateur> findByRole(Role role);
 }
